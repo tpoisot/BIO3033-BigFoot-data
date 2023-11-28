311bb6b2eff14ddd3b8b68cfdc05bd1739198f10
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-11-27 20:02:25 -0500

    ajout de oob

--- code/bootstrap.jl
+++ code/bootstrap.jl
@@ -1,8 +1,8 @@
-function bootstrap(y, X; n = 50)
+function bootstrap(y, X; n=50)
     @assert size(y, 1) == size(X, 2)
     bags = []
-    for i in 1:n
-        inbag = sample(1:size(X, 2), size(X, 2); replace = true)
+    for _ in 1:n
+        inbag = sample(1:size(X, 2), size(X, 2); replace=true)
         outbag = setdiff(axes(X, 2), inbag)
         push!(bags, (inbag, outbag))
     end
@@ -11,7 +11,7 @@ end
 
 mutable struct Bagging
     model::SDM
-    bags::Vector{Tuple{Vector{Int64}, Vector{Int64}}}
+    bags::Vector{Tuple{Vector{Int64},Vector{Int64}}}
     models::Vector{SDM}
 end
 
@@ -20,7 +20,7 @@ function Bagging(model::SDM, bags::Vector)
 end
 
 function train!(ensemble::Bagging, y, X; kwargs...)
-    ensemble.models = [train!(deepcopy(ensemble.model), y, X[:,bag[1]]; kwargs...) for bag in ensemble.bags]
+    ensemble.models = [train!(deepcopy(ensemble.model), y, X[:, bag[1]]; kwargs...) for bag in ensemble.bags]
     return ensemble
 end
 
@@ -28,4 +28,21 @@ function StatsAPI.predict(ensemble::Bagging, X; consensus=median, kwargs...)
     ŷ = [predict(component, X; kwargs...) for component in ensemble.models]
     ỹ = vec(mapslices(consensus, hcat(ŷ...); dims=2))
     return isone(length(ỹ)) ? only(ỹ) : ỹ
+end
+
+function outofbag(ensemble::Bagging, y, X, bags, args...; kwargs...)
+    instance = rand(eachindex(y))
+    done_instances = Int64[]
+    outcomes = Bool[]
+
+    for instance in eachindex(y)
+        valid_models = findall(x -> !(instance in x[1]), bags)
+        if !isempty(valid_models)
+            push!(done_instances, instance)
+            pred = [predict(ensemble.models[i], X[variables, instance], args...; kwargs...) for i in valid_models]
+            push!(outcomes, count(pred) > count(pred) // 2)
+        end
+    end
+
+    return ConfusionMatrix(outcomes, y[done_instances])
 end
